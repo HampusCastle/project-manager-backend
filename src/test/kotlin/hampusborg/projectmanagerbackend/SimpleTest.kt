dbb3a0d6ad930016b5bbcf2899eaff8ce070be24
HampusCastle <Hampuscastle@gmail.com>
2024-12-05 15:32:52 +0100

    Add simple test for GitHub Actions

---
+++ src/test/kotlin/hampusborg/projectmanagerbackend/SimpleTest.kt
@@ -0,0 +1,11 @@
+package hampusborg.projectmanagerbackend
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class SimpleTest {
+    @Test
+    fun simpleTest() {
+        Assertions.assertTrue(true, "Test passed!")
+    }
+}
